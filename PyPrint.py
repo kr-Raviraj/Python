55a46dcbb7000a99ac99e746c6ea8e7c20422f4c
kr.Raviraj Singh <50172851+kr-Raviraj@users.noreply.github.com>
2019-05-03 09:52:27 -0400

    Add files via upload

---
+++ PyPrint.py
@@ -0,0 +1,31 @@
+#Print Method In Python
+
+print("Python 3 has the print as a function where as in python 2 its a keyword ")
+
+print("keyword can be checked using (help) in terminal in both versions  ")
+
+print(" ")
+
+print("separate""car") # output = separatecar
+
+#print("separate"car") it will show invalid syntax
+
+#print(""separate""car"") It will also be an error
+
+print("'separate " "\car'") # output = 'separate \car'
+
+print("'separate' 'car'")   #output = 'separate' 'car'
+
+print('separate'r'car')     #output = separatecar
+
+print('separate"r"car')     #output = separate"r"car
+
+print("its \"a\"")          #output = its "a"
+
+#print "It is in version 2"
+#print'its in version 2'
+
+a=5
+print("c =",a+5)
+#print "a+b",a+5
+#print (eval('a+5'))
